840a3879ac10d4f7709bab513b12c06cd1f84e53
Ashashokh123 <85626972+Ashashokh123@users.noreply.github.com>
2026-04-17 22:42:25 +0530

    javaScript basics

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+// if (js === "amazing") alert("js is fun");
+console.log(40 + 66 + 5 - 7);
+
+let countryName = "India";
+let continent = "Asia";
+let population = 150,
+  million;
+let isBoolean = false;
+let countryNamee = India;
+
+console.log(countryName, continent, population);
+console.log(typeof countryName);
+console.log(typeof population);
+console.log(typeof isBoolean);
