4703e3b1cbd30b79d276618c767201e06e47ae28
Ashashokh123 <85626972+Ashashokh123@users.noreply.github.com>
2026-04-20 07:09:22 +0530

    variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,42 @@
-let js = "amazing";
-// if (js === "amazing") alert("js is fun");
-console.log(40 + 66 + 5 - 7);
+// let js = "amazing";
+// // if (js === "amazing") alert("js is fun");
+// console.log(40 + 66 + 5 - 7);
 
 let countryName = "India";
 let continent = "Asia";
-let population = 150,
-  million;
 let isBoolean = false;
 let isIsland = false;
-let language;
+let language = "telugu";
 
-console.log(countryName, continent, population);
-console.log(typeof countryName);
-console.log(typeof population);
-console.log(typeof isBoolean);
-console.log(typeof isIsland);
-console.log(typeof language);
+// console.log(countryName, continent, population);
+// console.log(typeof countryName);
+// console.log(typeof population);
+// console.log(typeof isBoolean);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+let age = "Ashok";
+age = 32;
+//   only initilization works in const
+const birthCountry = "India";
+console.log(birthCountry);
+
+var job = "programmer";
+job = "teacher";
+
+var amount;
+amount = 1000;
+
+const now = 2027;
+const ageAshok = now - 2000;
+const agelahari = now - 2003;
+console.log(ageAshok, agelahari);
+console.log(ageAshok / 2, ageAshok * 2);
+
+const firstName = "Ashok";
+const lastName = "Siri";
+console.log(firstName + " " + lastName);
+
+// x += 10; //x = x + 10'
+
+// x++; //x = x+1;
+// x++; //x = x-1;
