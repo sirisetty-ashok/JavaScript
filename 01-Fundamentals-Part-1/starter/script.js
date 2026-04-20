46909ebfb0bfbb088e9de929eaa98c9c9b0fba94
Ashashokh123 <85626972+Ashashokh123@users.noreply.github.com>
2026-04-20 07:42:45 +0530

    challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -59,3 +59,29 @@ const description =
   language +
   ".";
 console.log(description);
+
+// challenge 1
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+BMIMark = massMark / (heightMark * heightMark);
+BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
