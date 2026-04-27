eee60f863e352ffa5a0e9afdbd2a7966d7e9ddeb
Ashashokh123 <85626972+Ashashokh123@users.noreply.github.com>
2026-04-28 05:05:11 +0530

    using strict mode

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -277,9 +277,14 @@
 //   );
 // }
 
-const bill = Number(prompt("Enter the bill"));
+// const bill = Number(prompt("Enter the bill"));
 
-const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
-console.log(
-  ` the bill was ${bill}, the tip was ${tip}, and the total was ${bill + tip}`,
-);
+// const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+// console.log(
+//   ` the bill was ${bill}, the tip was ${tip}, and the total was ${bill + tip}`,
+// );
+
+//  to use strict mode we have to use this
+"use strict";
+username = "Ashok";
+console.log(username);
